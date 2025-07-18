a5aff222e4c3695e6c174c62f1cfffc703ebb1c9
Nurhidayah <Nurhidayah14@example.com>
2025-07-18 22:17:30 +0800

    Add tugas 2 - 3x3 interactive image grid

--- app/tugas2.tsx
+++ app/tugas2.tsx
@@ -13,29 +13,27 @@ const numColumns = 3;
 const imageSize = width / numColumns - 10;
 
 const mainImages = [
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr1XsNfIizwWFEn88paZUMAPPbyrO-NN3pzg&s=1',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRXMTY9oIoj2sCREKMBhGV7IhHO5c1OidrRg&s=2',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgd42TOgOHS5h89YC8DblEUKFBTHr139oUcA&s=3',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ25LsWbF327Fqw2qvaTdNa_Csq5qshxUtu8g&s=4',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRm8lyb1JF6G1t4C2ZjZbQfHrQBKoGs1jM7IQ&s=5',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRD0MSuFi0Q1ByyivJt748hmnTFbRXhhMnQIw&s=6',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2WHJgzaig4cgtcpPZYpGFeuu3KkX4q6WwcQ&s=7',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBdLuOIfj9QEk45q-9epbt-AGuLnnxoFnQRA&s=8',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_2nbnqGo20fuq6Wbj_4qq_Efp6w3VuH-PEmXh0lJFRI59myPDEkzqrjWQGp7KFxpI51Y&usqp=CAU=9',
-
+   'https://picsum.photos/id/1005/200',
+  'https://picsum.photos/id/1010/200',
+  'https://picsum.photos/id/1016/200',
+  'https://picsum.photos/id/1022/200',
+  'https://picsum.photos/id/1025/200',
+  'https://picsum.photos/id/1031/200',
+  'https://picsum.photos/id/1035/200',
+  'https://picsum.photos/id/1040/200',
+  'https://picsum.photos/id/1045/200',
 ];
 
 const altImages = [
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDdyeAST_pagLDYl02LtEkGJCWoFJ1FdLdog&s=10',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTd_mSy-TD-tUtVpxCL6Ri4sIO4C8xJ0wl77g&s=11',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR2VV8rX9jXr7zQ84DpbxkSY-o-AzE3X_RjIw&s=12',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTguylPbKIbClkVEyoM4E18gPNlxl5eIVS1IQ&s=13',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxwB_RMiwzKuDPYhBs4sFUi5Bpt-oGHcurDg&s=14',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS45BtbUKdysPQlHNLCnza5Zz97n0A0BCHJPw&s=15',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM4yhOXh3_D00b9t-ew73CchdOYWjhxocIEg&s=16',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH4XYwcwAKjyriyZHcx4MQwmAlDH-53ExUUA&s=17',
-  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpuUqPbUW21RoaisGUjW9daBV0XZwX2HJiwg&s=18',
-
+  'https://picsum.photos/id/1060/200',
+  'https://picsum.photos/id/1061/200',
+  'https://picsum.photos/id/1062/200',
+  'https://picsum.photos/id/1063/200',
+  'https://picsum.photos/id/1064/200',
+  'https://picsum.photos/id/1065/200',
+  'https://picsum.photos/id/1066/200',
+  'https://picsum.photos/id/1067/200',
+  'https://picsum.photos/id/1068/200',
 ];
 
 interface ImageState {
